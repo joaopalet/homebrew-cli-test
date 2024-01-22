2c94edb842b207998a76c89d9c2b67fd96b7538b
joaopalet <joao.palet@outlook.com>
2024-01-22 13:35:02 +0000

    Brew formula update for cli-test version v0.0.15

--- cli-test.rb
+++ cli-test.rb
@@ -5,21 +5,21 @@
 class CliTest < Formula
   desc "A test CLI built by JP"
   homepage "https://github.com/joaopalet/cli-test"
-  version "0.0.12"
+  version "0.0.15"
   license "Apache-2.0"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/joaopalet/cli-test/releases/download/v0.0.12/cli-test_Darwin_arm64.tar.gz"
-      sha256 "c122208f938f9e778a8c6bf93df6c002c9fefea52d0bc01b2b7c3a77daddd22c"
+    if Hardware::CPU.intel?
+      url "https://github.com/joaopalet/cli-test/releases/download/v0.0.15/cli-test_Darwin_x86_64.tar.gz"
+      sha256 "9261aeeb4a2e6dab5ea6c383f05907f2172f4981d26aa89808d6e6ec80286642"
 
       def install
         bin.install "jp-cli"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/joaopalet/cli-test/releases/download/v0.0.12/cli-test_Darwin_x86_64.tar.gz"
-      sha256 "8ee32455eec704a7331ee028b69e9f38b50f77d2d7d6ebc272d9ccb3c9092099"
+    if Hardware::CPU.arm?
+      url "https://github.com/joaopalet/cli-test/releases/download/v0.0.15/cli-test_Darwin_arm64.tar.gz"
+      sha256 "ed9c9fc31d8c6fb5dfdb7cff9c7e1ae60bd7be7f49a69dad73647badfbdf70e2"
 
       def install
         bin.install "jp-cli"
@@ -29,16 +29,16 @@ class CliTest < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/joaopalet/cli-test/releases/download/v0.0.12/cli-test_Linux_arm64.tar.gz"
-      sha256 "136ed41252625e3cb23fe5f4fe040d29ecc6f74dc9c317284c3ef6710c5fb6bf"
+      url "https://github.com/joaopalet/cli-test/releases/download/v0.0.15/cli-test_Linux_arm64.tar.gz"
+      sha256 "2a9081ca5560d7e2ea482c799e3127849b5219ab41a3bd09bc0ef8add34ec77b"
 
       def install
         bin.install "jp-cli"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/joaopalet/cli-test/releases/download/v0.0.12/cli-test_Linux_x86_64.tar.gz"
-      sha256 "1743a0d88550c3662a3ca5a79013c1f4b41527ba2aa393ea1c882c552724af19"
+      url "https://github.com/joaopalet/cli-test/releases/download/v0.0.15/cli-test_Linux_x86_64.tar.gz"
+      sha256 "e3c8f7312199da51a59d00963092e09d8d1be31301769ed6beaee4453e457b6b"
 
       def install
         bin.install "jp-cli"
